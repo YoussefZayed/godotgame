2eae0f03666468e1ac218ae626712fe3efcd2393
superemma13 <144706228+superemma13@users.noreply.github.com>
2024-03-10 00:18:16 -0500

    melee combat animations
    press LMB to attack (just animation)

--- player.gd
+++ player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 @export var speed = 200.0
 var direction = Vector2.ZERO
 @onready var anim = $AnimatedSprite2D
-@onready var weapon = $WeaponAnimation
+@onready var weapAnim = $AnimationPlayer
+@onready var weapon = $Weapon
+
+func _ready():
+	weapon.visible = false
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -49,4 +53,15 @@ func _process(delta):
 	position += velocity * delta
 	
 	if Input.is_action_just_pressed("attack_melee"):
-		weapon.play()
+		weapon.visible = true
+		if direction == Vector2.RIGHT:
+			weapAnim.play("ruler_attack_right");
+		elif direction == Vector2.LEFT:
+			weapAnim.play("ruler_attack_left");
+		elif direction == Vector2.UP:
+			weapAnim.play("ruler_attack_back")
+		else:
+			weapAnim.play("ruler_attack_front")
+		
+	await weapAnim.animation_finished
+	weapon.visible = false
