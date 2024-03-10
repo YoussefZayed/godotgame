a0f075b9df10656b7adb5432beb613641395914a
Charles <yicharles04@gmail.com>
2024-03-10 15:58:03 -0400

    floor background

---
+++ pause.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
+
+
+func _on_resume_pressed():
+	pass # Replace with function body.
