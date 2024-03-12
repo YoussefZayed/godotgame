454a1b7c289f2779a3d53113f0ba4691c6e86995
Charles <yicharles04@gmail.com>
2024-03-12 17:39:46 -0400

    switched from rigidbody2d to charcterbody2d

--- player.gd
+++ player.gd
@@ -1,11 +1,11 @@
-extends RigidBody2D
+extends CharacterBody2D
 class_name Player
 
 @export var speed = 200.0
 @export var maxHealth = 100
 @export var money = 0
 @export var health = 100
-var direction = Vector2.ZERO
+var dir = Vector2.ZERO
 @onready var anim = $AnimatedSprite2D
 @onready var weapAnim = $AnimationPlayer
 @onready var weapon = $Weapon
@@ -24,57 +24,60 @@ func _ready():
 	weaponCollision.set_deferred("disabled", true)
 
 func _process(delta):
-	var velocity = Vector2.ZERO
+	var direction = Input.get_vector("move_left", "move_right","move_up", "move_down").normalized()
+	
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
 	
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		direction = Vector2.RIGHT
+		dir = Vector2.RIGHT
+		
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		direction = Vector2.LEFT
+		dir = Vector2.LEFT
+		
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		direction = Vector2.UP
+		dir = Vector2.UP
+		
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-		direction = Vector2.DOWN
-	
+		dir = Vector2.DOWN
+	#
 	# Dictate all the animations
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		if direction == Vector2.UP:
+		if dir == Vector2.UP:
 			anim.play("walk_back")
-		elif direction == Vector2.DOWN:
+		elif dir == Vector2.DOWN:
 			anim.play("walk_front")
-		elif direction == Vector2.LEFT:
+		elif dir == Vector2.LEFT:
 			anim.play("walk_side");
 			anim.flip_h = true
 		else:
 			anim.play("walk_side");
 			anim.flip_h = false
 	else:
-		if direction == Vector2.RIGHT:
+		if dir == Vector2.RIGHT:
 			anim.play("idle_side");
 			anim.flip_h = false
-		elif direction == Vector2.LEFT:
+		elif dir == Vector2.LEFT:
 			anim.play("idle_side");
 			anim.flip_h = true
-		elif direction == Vector2.UP:
+		elif dir == Vector2.UP:
 			anim.play("idle_back")
 		else:
 			anim.play("idle_front")
 	
-	if velocity != Vector2.ZERO:
-		position += velocity * delta
 	
 	if Input.is_action_just_pressed("attack_melee"):
 		weapon.visible = true
 		weaponCollision.set_deferred("disabled", false)
-		if direction == Vector2.RIGHT:
+		if dir == Vector2.RIGHT:
 			weapAnim.play("ruler_attack_right");
-		elif direction == Vector2.LEFT:
+		elif dir == Vector2.LEFT:
 			weapAnim.play("ruler_attack_left");
-		elif direction == Vector2.UP:
+		elif dir == Vector2.UP:
 			weapAnim.play("ruler_attack_back")
 		else:
 			weapAnim.play("ruler_attack_front")
