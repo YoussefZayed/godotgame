3a020ac2c45ecb8985af3b4473a6aaedd1722bbb
Charles <yicharles04@gmail.com>
2024-03-15 17:12:33 -0400

    pause menu

--- main.gd
+++ main.gd
@@ -6,7 +6,3 @@ extends Node2D
 func _ready():
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	if Input.is_action_just_pressed("pause"):
-		pass
--- pause.gd
+++ pause.gd
@@ -1,19 +1,41 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("RESET")
 
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://options.tscn")
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func esc():
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
 
 
 func _on_resume_pressed():
-	pass # Replace with function body.
+	resume()
+
+
+func _on_restart_pressed():
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_quit_to_main_pressed():
+	get_tree().quit()
+
+
+
+func _process(_delta):
+	esc()
+
+
+func _on_main_menu_pressed():
+	resume()
+	get_tree().change_scene_to_file("res://menu.tscn")
