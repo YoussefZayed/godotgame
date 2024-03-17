d1553b637d108a3d04d0967a6715908c7c9bdffa
superemma13 <144706228+superemma13@users.noreply.github.com>
2024-03-17 16:13:06 -0400

    wizard enemy beginning

---
+++ wizard_enemy.gd
@@ -0,0 +1,67 @@
+extends RigidBody2D
+
+@export var speed = 10
+@export var knockbackForce = 100
+var player_chase = false
+var player = null
+var health = 30
+var spawnedCoin = false
+var isDieing = false
+@export var coin = preload("res://coin.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite2D.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player_chase and !isDieing:
+		var distance = player.position - position
+		if distance > 10:
+			var direction = (distance).normalized()
+			apply_impulse(direction * speed * delta)
+			position += (player.position - position)/(speed+45)
+			if (player.position.x - position.x) <0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
+	
+	if self.health <= 0 && !isDieing:
+		isDieing = true
+		self.death()
+
+func enemyHit(damage):
+	health -= damage
+	$EnemyHurt.play()
+
+func death():
+	$EnemyDeath.play()
+	player_chase = false
+	set_sleeping(true)
+	get_node("AnimatedSprite2D").play("death")
+	await get_node("AnimatedSprite2D").animation_finished
+	spawnCoin()
+	self.queue_free()
+
+func spawnCoin():
+	if !spawnedCoin:
+		spawnedCoin = true
+		var newCoin = coin.instantiate()
+		newCoin.position = self.position
+		get_parent().add_child(newCoin)
+
+func _on_detection_area_body_entered(body):
+	if body is Player:
+		player = body
+		player_chase = true
+
+func knockedBack():
+	var knockBack = (player.position - position).normalized()
+	apply_impulse(-knockBack * knockbackForce)
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "Ruler" && player:
+		self.enemyHit(player.damage)
+		print("Enemy health: ", health)
+		knockedBack()
