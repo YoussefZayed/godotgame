60aaafd3b3d3deaeb911493d5dc3b0c931253c8a
superemma13 <144706228+superemma13@users.noreply.github.com>
2024-03-19 00:53:20 -0400

    edits to try and fix

---
+++ room_a.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+#@onready var wizardEnemy = $WizardEnemy
+#@onready var projectileManager = $Projectiles
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#wizardEnemy.player_shot_projectile.connect(projectileManager.handleProjectiles)
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
