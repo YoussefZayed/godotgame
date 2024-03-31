4c8999f4f9efc65635995f56c9dc2614c8dea46a
superemma13 <144706228+superemma13@users.noreply.github.com>
2024-03-31 15:10:21 -0400

    credits page

--- menu.gd
+++ menu.gd
@@ -10,3 +10,7 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://credits.tscn")
